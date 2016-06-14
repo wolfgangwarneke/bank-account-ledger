86906edd9e2b1057f51fcda7e96fc60def1f506f
Joel Delight and Wolfgang Warneke <wolfgang@>
2016-06-14 14:46:33 -0700

    create the back-end logic that displays transaction history into the table.

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,27 @@ function Transaction(amount, comment) {
   this.time = new Date();
 };
 
+
+
 var accountOne = new Account("John Doe", 30000, "savings");
 
 
 $(function() {
-  
-})
+  Account.prototype.updateLedger = function() {
+    $('#ledger').empty();
+    var ledgerBalance = 0;
+    this.transactionLog.forEach(function(transaction) {
+      console.log('works');
+      ledgerBalance += transaction.amount;
+      $('#ledger').prepend('<tr><td>' + transaction.amount + '</td><td>' + transaction.comment + '</td><td>' + transaction.time + '</td><td>' + ledgerBalance + '</td></tr>');
+    });
+  }
+
+  accountOne.updateLedger();
+
+  $('#deposit').click(function() {
+
+    accountOne.accountTransaction( parseFloat(parseFloat($('#transaction-amount').val()).toFixed(2)), $('#transaction-comment').val() );
+    accountOne.updateLedger();
+  });
+});
